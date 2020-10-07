88291486c37f98099376f168bfb36f96823115e5
Tharuka Sandaru Jayarathna <tjtharuka5@gmail.com>
2020-10-07 22:09:50 +0530

    Interface changes in homepage

--- client/src/components/contacts/Contact.js
+++ client/src/components/contacts/Contact.js
@@ -4,31 +4,35 @@ import ContactContext from '../../context/contact/contactContext';
 import Spinner from '../layouts/Spinner';
 
 const Contact = () => {
-    const contactContext = useContext(ContactContext);
-    const { contacts, filtered, getContacts, loading } = contactContext;
+  const contactContext = useContext(ContactContext);
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-    useEffect(() => {
-        getContacts();
-        // eslint-disable-next-line
-    }, [])
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
 
-    if (contacts !== null && contacts.length === 0 && !loading) {
-        return <div className="h3">Please add a contact</div>
-    }
+  if (contacts !== null && contacts.length === 0 && !loading) {
+    return <div className='h3'>Please add a contact</div>;
+  }
 
-    return (
-        <Fragment>
-            {contacts !== null && !loading ? (
-                <div>
-                    {filtered !== null ? filtered.map(contact =>
-                        (<ContactItem key={contact._id} contact={contact} />)
-                    ) : contacts.map(contact => (
-                        <ContactItem key={contact._id} contact={contact} />
-                    ))}
-                </div>
-            ) : <Spinner />}
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      {contacts !== null && !loading ? (
+        <div className='d-flex justify-content-around flex-wrap m-2'>
+          {filtered !== null
+            ? filtered.map((contact) => (
+                <ContactItem key={contact._id} contact={contact} />
+              ))
+            : contacts.map((contact) => (
+                <ContactItem key={contact._id} contact={contact} />
+              ))}
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  );
+};
 
 export default Contact;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,28 +5,35 @@ import FilterContact from '../contacts/FilterContact';
 import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  const { loadUser } = authContext;
 
-    const authContext = useContext(AuthContext);
-    const { loadUser } = authContext;
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(() => {
-        loadUser();
-        // eslint-disable-next-line
-    }, []);
-
-    return (
-        <div className="container d-flex justify-content-center">
-            <div className="row w-75">
-                <div className="col">
-                    <ContactForm />
-                </div>
-                <div className="col">
-                    <FilterContact />
-                    <Contacts />
-                </div>
-            </div>
+  return (
+    <div className='container'>
+      <div>
+        <div className='w-50 ml-auto mr-auto'>
+          <ContactForm />
         </div>
-    )
-}
+        <div className='mt-5'>
+          <hr />
+          <h2 className='text-center mt-3'>
+            Your <span className='text-primary'>Contacts</span>
+          </h2>
+          <div className='w-50'>
+            <FilterContact />
+          </div>
+          <div>
+            <Contacts />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
